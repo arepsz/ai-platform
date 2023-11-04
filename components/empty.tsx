b82196915f5a3a987342bd862b32ec4fde314453
Farkas Árpád <asderasderasd163@gmail.com>
2023-11-04 18:11:28 +0100

    First commit

---
+++ components/empty.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image"
+
+interface EmptyProps {
+    label: string
+}
+
+export const Empty = ({
+    label
+}: EmptyProps) => {
+    return (
+        <>
+            <div className="h-fill p-20 flex flex-col items-center
+            justify-center">
+                <div className="relative h-72 w-80">
+                    <Image 
+                        alt="Empty"
+                        fill
+                        src="/empty.png"
+                    />
+                </div>
+            </div>
+            <p className="text-muted-foreground text-sm text-center">
+                {label}
+            </p>
+            <div className="flex items-center justify-center">
+                <footer className="sticky top-[100vh] text-yellow-500/50">
+                    <a target="_blank" href='https://pngtree.com/freepng/social-media-concept-in-3d-isometric-outline-design-woman-browses-on-laptop-creates-profile-watches-video-content-likes-and-shares-posts-line-web-template-vector-illustration-with-people-scene_7601730.html'>png image from pngtree.com/</a>
+                </footer>
+            </div>
+        </>
+    )
+}
